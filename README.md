2a4417484c01bc0cb4bb929346d8f2a07413f96f
Gordon Weakliem <gordon.weakliem@gmail.com>
2023-08-11 13:14:09 -0600

    clean up Args

---
+++ README.md
@@ -0,0 +1,8 @@
+# fileorganizer
+
+I wanted to learn some Rust, and I have a terabyte drive full of old pictures that are super disorganized across many directories. My idea is to try to determine which of these pictures are duplicates and attempt to organize them.
+
+Duplicate detection by 
+* File Name
+* SHA-256 hash
+* EXIF Metadata
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,12 @@ use clap::Parser;
 use std::path::Path;
 use std::fs;
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Directory to organize
     #[arg(short, long)]
     dir: String,
-
-    // / Number of times to greet
-    // #[arg(short, long, default_value_t = 1)]
-    // count: u8,
 }
 
 fn organize(dir: &Path) -> i32 {
